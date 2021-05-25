5036eb44d6b7e17878429cdc242fbe235232ac23
tontsa28 <xdman7917@gmail.com>
2021-05-25 20:48:36 +0300

    The project is back alive after a long break!

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-var character = document.getElementById("mario");
-var block = document.getElementById("block");
-function jump(){
-    if(character.classList != "animate"){
-        character.classList.add("animate");
+const mario = document.getElementById("mario")
+
+function jump() {
+    if (mario.classList != "jump") {
+        mario.classList.add("jump");
+        setTimeout(function () {
+            mario.classList.remove("jump");
+        }, 500);
     }
-    setTimeout(function(){
-        character.classList.remove("animate");
-    },500);
 }
 
-var checkDead = setInterval(function(){
-    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>0 && characterTop>=130){
-        block.style.animation = "none";
-        block.style.display = "none";
-        alert("YOU LOST");
-    }
-},10);
+document.addEventListener("keydown", function (event) {
+    jump();
+});
