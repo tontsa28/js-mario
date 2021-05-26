79743192a6c9f677c0766dc1114185849df73a27
tontsa28 <xdman7917@gmail.com>
2021-05-26 17:16:24 +0300

    Added new assets and added box colliders

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # js-mario
-Very simple, Chrome-inspired Mario jumping browser game in JavaScript. Still in development, release 1.0 will be available as soon as i will finish it.
+Very simple, Chrome-inspired Mario jumping browser game made with HTML, CSS and JavaScript.
+
+![js-mario](assets/game_screenshot.png)
--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const mario = document.getElementById("mario")
+const mario = document.getElementById("mario");
+const pipe = document.getElementById("pipe");
 
 function jump() {
     if (mario.classList != "jump") {
         mario.classList.add("jump");
         setTimeout(function () {
             mario.classList.remove("jump");
-        }, 500);
+        }, 700);
     }
 }
+
+let isAlive = setInterval(function () {
+    let marioTop = parseInt(window.getComputedStyle(mario).getPropertyValue("top"));
+    let pipeLeft = parseInt(window.getComputedStyle(pipe).getPropertyValue("left"));
+
+    if (pipeLeft <50 && pipeLeft > 0 && marioTop >= 245) {
+        alert("Game over!");
+    }
+}, 10);
 
 document.addEventListener("keydown", function (event) {
     jump();
